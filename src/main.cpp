db2809912578f140537dcd455f4fca49607a4737
Strooom <pascal.roobrouck@gmail.com>
2022-10-20 08:54:02 +0200

    new version, hanldng non ASCII input

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,58 @@
-#include "msgbuffer.h"
 #include "Arduino.h"
 
-msgBuffer buffer2;
-msgBuffer buffer3;
-
-void setup()
-{
-  Serial.begin(115200);
-  Serial.flush();
-  delay(1000);
-  Serial.println("\n\nSerialSpy started\n\n");
-  Serial2.begin(115200);
-  Serial3.begin(115200);
+void setup() {
+    Serial.begin(115200);
+    Serial.flush();
+    delay(1000);
+    Serial.println("\n\nSerialSpy started\n\n");
+    Serial2.begin(115200);
+    Serial3.begin(115200);
 }
 
-void loop()
-{
-  while (Serial2.available() > 0)
-  {
-    char lastCharacter;
-    lastCharacter = Serial2.read();
-    if (lastCharacter == '\n')
-    {
-      buffer2.buffer[buffer2.level] = 0x00;
-      Serial.printf("MCU -->> PC : %s", buffer2.buffer);
-      buffer2.level = 0;
+void loop() {
+    while (Serial3.available() > 0) {
+        char lastCharacter;
+        lastCharacter = Serial3.read();
+        switch (lastCharacter) {
+            case '?':
+            case '!':
+            case '~':
+                Serial.println(lastCharacter);
+                break;
+            case 0x18:
+                Serial.printf("0x%02x\n", lastCharacter);
+                break;
+            case 0x0A:
+            case 0x0D:
+                Serial.println("");
+                break;
+            default:
+                if ((lastCharacter <= 32) || (lastCharacter >= 127)) {
+                    Serial.printf("0x%02x ", lastCharacter);
+                } else {
+                    Serial.print(lastCharacter);
+                }
+                break;
+        }
     }
-    else
-    {
-      buffer2.buffer[buffer2.level] = lastCharacter;
-      buffer2.level++;
-    }
-  }
 
-  while (Serial3.available() > 0)
-  {
-    char lastCharacter;
-    lastCharacter = Serial3.read();
-    if (lastCharacter == '\n')
-    {
-      buffer3.buffer[buffer3.level] = 0x00;
-      Serial.printf("  MCU <<-- PC : %s", buffer3.buffer);
-      buffer3.level = 0;
+    while (Serial2.available() > 0) {
+        char lastCharacter;
+        lastCharacter = Serial2.read();
+        switch (lastCharacter) {
+            case 0x0A:
+                Serial.println("LF");
+                break;
+            case 0x0D:
+                Serial.println("CR");
+                break;
+            default:
+                if ((lastCharacter <= 32) || (lastCharacter >= 127)) {
+                    Serial.printf("0x%02x ", lastCharacter);
+                } else {
+                    Serial.print(lastCharacter);
+                }
+                break;
+        }
     }
-    else
-    {
-      buffer3.buffer[buffer3.level] = lastCharacter;
-      buffer3.level++;
-    }
-  }
 }
